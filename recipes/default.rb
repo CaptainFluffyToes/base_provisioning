5512228b88624e498e8e7879f478aa306a439762
CaptainFluffyToes <DJKhan85@gmail.com>
2017-10-28 08:16:47 -0400

    Cleaned up code style

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook:: base_configuration
 # Recipe:: default
 #
-# Copyright:: 2017, The Authors, All Rights Reserved.
+# Copyright:: 2017, Darren Khan, All Rights Reserved.
 
 node.default['chef_client']['interval'] = '900'
 node.default['chef_client']['splay'] = '900'
--- recipes/setup.rb
+++ recipes/setup.rb
@@ -5,19 +5,19 @@
 # Copyright:: 2017, Darren Khan, All Rights Reserved.
 
 if node['platform'] == 'debian'
-	installs = ['apt-transport-https']
-	installs.each do |install|
-		package install do
-			action :install
-		end
-	end
-	apt_update 'Update Apt' do
-		action :update
-	end
-	installs = ['sudo', 'open-vm-tools', 'curl', 'git']
-	installs.each do |install|
-		package install do
-			action :install
-		end
-	end
+  installs = ['apt-transport-https']
+  installs.each do |install|
+    package install do
+      action :install
+    end
+  end
+  apt_update 'Update Apt' do
+    action :update
+  end
+  installs = ['sudo', 'open-vm-tools', 'curl', 'git']
+  installs.each do |install|
+    package install do
+      action :install
+    end
+  end
 end
